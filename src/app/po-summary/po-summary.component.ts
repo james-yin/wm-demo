c801e5deb0fee3ced590a294be131180b9d9b658
James Yin <jyin88@gmail.com>
2018-04-12 21:19:50 -0700

    bind summary data to table

--- src/app/po-summary/po-summary.component.ts
+++ src/app/po-summary/po-summary.component.ts
@@ -9,15 +9,45 @@ import * as _ from "lodash";
   templateUrl: './po-summary.component.html'
 })
 export class PoSummaryComponent implements OnInit {
+  cols: any[];
   poSummary: any[];
+  selectedSummary: any;
+  displayDialog = false;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.getPoSummaryData();
+    this.setUpCols();
+  }
+
+  setUpCols() {
+    this.cols = [
+      { field: 'materialNumber', header: '物料编号' },
+      { field: 'imageFile', header: '图档' },
+      { field: 'isSample', header: '样品' },
+      { field: 'specifications', header: '规格型号' },
+      { field: 'materialName', header: '物料名称' },
+      { field: 'purchaseUnit', header: '请购单位' },
+      { field: 'purchaseQuantity', header: '请购数量' },
+      { field: 'standardUnit', header: '标准单位' },
+      { field: 'standardQuantity', header: '标准数量' },
+      { field: 'deliveryLocation', header: '送货地点' },
+      { field: 'packaging', header: '包裝' },
+      { field: 'recommendedSupplier', header: '建议供应商' },
+      { field: 'isOpenOrder', header: '已開訂單' },
+    ];
+  }
+
+  getPoSummaryData() {
     this.dataService.getPoSummaryData().subscribe(data => {
       this.poSummary = data;
       console.log(this.poSummary);
     });
+  }
+
+  onRowSelect(event) {
+    this.displayDialog = true;
   }
 
 }
