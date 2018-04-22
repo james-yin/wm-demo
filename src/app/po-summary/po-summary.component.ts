7b1f5627ab52b36f18bdb53c3e38897b74f20f3c
James Yin <jyin88@gmail.com>
2018-04-22 11:24:01 -0700

    show dialogs for images and quotes

--- src/app/po-summary/po-summary.component.ts
+++ src/app/po-summary/po-summary.component.ts
@@ -34,7 +34,11 @@ export class PoSummaryComponent implements OnInit {
       accept: () => {
         this.displayDialog = false;
         this.msgs = [
-          { severity: 'info', summary: '确认', detail: '此记录已删除' }
+          {
+            severity: 'info',
+            summary: '确认',
+            detail: '此记录已删除'
+          }
         ];
       }
     });
@@ -42,19 +46,58 @@ export class PoSummaryComponent implements OnInit {
 
   setUpCols() {
     this.cols = [
-      { field: 'materialNumber', header: '物料编号' },
-      { field: 'imageFile', header: '图档' },
-      { field: 'isSample', header: '样品' },
-      { field: 'specifications', header: '规格型号' },
-      { field: 'materialName', header: '物料名称' },
-      { field: 'purchaseUnit', header: '请购单位' },
-      { field: 'purchaseQuantity', header: '请购数量' },
-      { field: 'standardUnit', header: '标准单位' },
-      { field: 'standardQuantity', header: '标准数量' },
-      { field: 'deliveryLocation', header: '送货地点' },
-      { field: 'packaging', header: '包裝' },
-      { field: 'recommendedSupplier', header: '建议供应商' },
-      { field: 'isOpenOrder', header: '已開訂單' }
+      {
+        field: 'materialNumber',
+        header: '物料编号'
+      },
+      {
+        field: 'imageFile',
+        header: '图档'
+      },
+      {
+        field: 'isSample',
+        header: '样品'
+      },
+      {
+        field: 'specifications',
+        header: '规格型号'
+      },
+      {
+        field: 'materialName',
+        header: '物料名称'
+      },
+      {
+        field: 'purchaseUnit',
+        header: '请购单位'
+      },
+      {
+        field: 'purchaseQuantity',
+        header: '请购数量'
+      },
+      {
+        field: 'standardUnit',
+        header: '标准单位'
+      },
+      {
+        field: 'standardQuantity',
+        header: '标准数量'
+      },
+      {
+        field: 'deliveryLocation',
+        header: '送货地点'
+      },
+      {
+        field: 'packaging',
+        header: '包裝'
+      },
+      {
+        field: 'recommendedSupplier',
+        header: '建议供应商'
+      },
+      {
+        field: 'isOpenOrder',
+        header: '已開訂單'
+      }
     ];
   }
 
@@ -70,6 +113,12 @@ export class PoSummaryComponent implements OnInit {
 
   save() {
     this.displayDialog = false;
-    this.msgs = [{ severity: 'info', summary: '确认', detail: '此记录已保存' }];
+    this.msgs = [
+      {
+        severity: 'info',
+        summary: '确认',
+        detail: '此记录已保存'
+      }
+    ];
   }
 }
